e52437c45cd4eb67050a20f463104a9ecc044f8e
Rodolfo Echenique <echeniquer@gmail.com>
2024-06-30 16:27:30 +1200

    Websocket support added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelephoneModule } from './telephone/telephone.module';
 import { ConfigModule } from '@nestjs/config';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TelephoneModule
+    TelephoneModule,
+    MessagesWsModule
   ],
   controllers: [AppController],
   providers: [AppService],
