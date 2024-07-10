d3734cd8cff81539f42ffdc748d23235a3df699b
Rodolfo Echenique <echeniquer@gmail.com>
2024-07-10 21:58:48 +1200

    Changes on telephoneActions.ts

--- src/plugins/telephoneActions.ts
+++ src/plugins/telephoneActions.ts
@@ -4,16 +4,5 @@ export const dialKey = async (key: string) => {
 
     const action = telephoneKeys[key];
     if(!action) return;
-    //console.log(`${key}: ${action}`);
-    // console.log(action);
-    try{
-        const url = `${base_url}${key}`;
-        const marcar = await fetch( url , {
-            headers: headers
-        });
-    }catch(e){
-        //console.log(e);
-        throw new Error('Error on telephone connection');
-    }
-
+    fetchAction(action);
 }
